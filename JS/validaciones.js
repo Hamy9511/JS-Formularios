298bc4ddd700d6dd316851c1996e12c7824e3b2a
Hamilton Durán <hamy.duran@gmail.com>
2023-04-18 23:44:33 -0600

    Añadimos mensajes de error en los campos del formulario

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -5,12 +5,55 @@ export function valida(input){ //Funcion para recibir inputs y va estaer vincula
     if(validadores[tipoDeInput]){ //Verofocar si en validadores existe un tipo de input
         validadores[tipoDeInput](input);
     }
+    //CON ESTO HACEMOS QUE SE MARQUE CON ROJO LOS INPUTS QUE ESTEN VACIOS LUEGO DE HACER UN FOCUS
+    //console.log(input.parentElement); //Para ver el eleento padre de mis input del HTMLM
+    if(input.validity.valid){ //Comando Validity hace que veamos toda la config del elemento pero en este caso nos interesa el "valid"
+        input.parentElement.classList.remove("input-container--invalid") //ClasssList para remover una clase cuando "valid" es true
+        input.parentElement.querySelector(".input-message-error").innerHTML = " "; //Insertamos al DOM del HTML un espacio vacio
+    }else{
+        input.parentElement.classList.add("input-container--invalid") //ClassList para agregar una clase cuando el "valid" es false
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
 }
+    const tipoDeErrores = ["valueMissing","typeMismatch","patternMismatch","customError"];
+    //CONFIGURANDO MENSAJES DE ERROR PARA CADA CAMPO NECESARIO
+    const mensajesDeError = { //Funcionn que emitira eroress
+        //trabajamos con los id de cada elemento.
+        nombre:{
+            valueMissing: "EL CAMPO NOMBRE NO PUEDE ESTAR VACIO" //Campo del validity cuando quitas el focus del campo sin llenar
+        },
+        email: {
+            valueMissing: "EL CAMPO CORREO NO PUEDE ESTAR VACIO",
+            typeMismatch: "EL CORREO NO ES VALIDO" //Campo del validity cuando te equivocas o no es valido el correo
+        },
+        
+        password: {
+            valueMissing: "EL CAMPO PASSWORD NO PUEDE ESTAR VACIO",
+            patternMissmatch: "Al menos una letra y un número" //Caudno no cumple el pattern de la contraseña
+        },
+        nacimiento: {
+            valueMissing: "EL CAMPO FECHA NO PUEDE ESTAR VACIO",
+            customError: "DEBES DE TENER 18 AÑOS" //Cuando valida que no tenes 18 años.
+        },
+    };
 
 const validadores = { //funcion que va a recibir el valor del elemmento data attributtes "Nacimiento"
     nacimiento: input => validarNacimiento(input), 
 };
 
+//EMITIR EL TEXTO DE ERROR
+function mostrarMensajeDeError(tipoDeInput, input){
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {// funcion que verifica cada uno de los campos del arreglo
+        if(input.validity[error]){ //Si es verdadero el valid de cada tipo de error
+            console.log(error); //Verificar que tipo d error esta leyendo
+            console.log(input.validity[error]); //Verificando si el valid es true o false
+            console.log(mensajesDeError[tipoDeInput][error]); //Verificamos que si esta leyendo el mensaje de error
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+    return mensaje;
+}
 //------------------------------------------------------------------------------------------------------------------------------------
 //const inputNacimiento = document.querySelector("#birth"); // 1 Seleecionamos el id del elemento donde estara la fecha
 
@@ -19,7 +62,7 @@ const validadores = { //funcion que va a recibir el valor del elemmento data att
    // console.log(event.target);
 //})
 
-function validarNacimiento(input){ // Validamos si el cleinaddte es mayor de edad
+function validarNacimiento(input){ // Validamos si el clientete es mayor de edad
     const fechaCliente = new Date (input.value); //convertimos el elemento captura en unicament euna fecha
     //console.log(input.value);
     let mensaje = ""; //Emitimos un mensaje en blanco que estara siempre  hasta que se cumpla la siguiente condicion
